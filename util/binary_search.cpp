3445860efb4420ab93f96e9b4ad51e905a2c5a79
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2025-12-28 21:55:42 +0530

    feat: add binary search

---
+++ util/binary_search.cpp
@@ -0,0 +1,22 @@
+// Binary Search
+
+bool check(int mid) {
+  if(true) return 1;
+  return 0;
+}
+
+int binary_search(const vi& nums) {
+  int lo = 0,hi = nums.size()-1;
+  int ans = -1;
+  while(lo <=hi) {
+    int mid = lo + (hi-lo)/2;
+    if(check(mid)) {
+       hi = mid-1;
+      ans = mid;
+    }
+    else {
+     lo = mid+1;
+    }
+  }
+  return ans;
+}
