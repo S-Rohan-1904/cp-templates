50544520d9b6b877710b7788dcfaa57bc0d9a850
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2025-12-28 21:56:11 +0530

    feat: add reduced fraction comparison

--- math/red_fractions.cpp
+++ math/red_fractions.cpp
@@ -3,9 +3,12 @@
  */
 
 pll get_red_fraction(ll a, ll b) {
-  if( a==0 && b==0) return mp(0,0);
-  else if(a==0) return mp(0,1);
-  else if(b==0) return mp(1,0);
+  if(a == 0 && b == 0) return mp(0LL,0LL);
+  else if(a == 0) return mp(0LL,1LL);
+  else if(b == 0) return mp(1LL,0LL);
+  int neg = 1;
+  if(a<0) {neg*=-1; a*=-1;}
+  if(b<0) {neg*=-1; b*=-1;}
   ll g = __gcd(a,b);
-  return mp(a/g,b/g);
+  return mp(neg*a/g,b/g);
 }
