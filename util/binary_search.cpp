ed152f2bac18d04149fce47fa7a10b5530d1e777
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2026-01-08 19:28:37 +0530

    fix: formatting

--- util/binary_search.cpp
+++ util/binary_search.cpp
@@ -8,15 +8,13 @@ bool check(int mid) {
 int binary_search(const vi& nums) {
   int lo = 0,hi = nums.size()-1;
   int ans = -1;
-  while(lo <=hi) {
+  while(lo <= hi) {
     int mid = lo + (hi-lo)/2;
     if(check(mid)) {
-       hi = mid-1;
+      hi = mid-1;
       ans = mid;
-    }
-    else {
-     lo = mid+1;
-    }
+    } else lo = mid+1;
+    
   }
   return ans;
 }
