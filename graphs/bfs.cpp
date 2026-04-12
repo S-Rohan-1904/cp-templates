8d9da0afb43e9bf58976b614d22c9a09b06db99f
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2026-04-13 02:28:27 +0530

    feat: floyd warshall

--- graphs/bfs.cpp
+++ graphs/bfs.cpp
@@ -1,16 +1,16 @@
-void bfs(int start_node) {
+void bfs(int start) {
     queue<int> q;
-    q.push(start_node);
-    dist[start_node] = 0;
+    q.push(start);
+    dist[start] = 0;
     while(!q.empty()) {
         int front = q.front();
         q.pop();
         if(vis[front]) continue;
         vis[front] = 1;
-        for(auto v : g[el]) {
-            if(!vis[v]) {
-              q.push(v);
-              dist[v] = dist[front] + 1;
+        for(auto child : g[front]) {
+            if(!vis[child]) {
+              q.push(child);
+              dist[child] = dist[front] + 1;
             }
         } 
     }
