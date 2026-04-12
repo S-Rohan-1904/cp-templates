ae7a263813bba09fe86858e543a5e71b2f784413
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2026-04-13 02:32:30 +0530

    fix: bfs

--- graphs/bfs.cpp
+++ graphs/bfs.cpp
@@ -2,13 +2,13 @@ void bfs(int start) {
     queue<int> q;
     q.push(start);
     dist[start] = 0;
+    vis[start] = 1;
     while(!q.empty()) {
         int front = q.front();
         q.pop();
-        if(vis[front]) continue;
-        vis[front] = 1;
         for(auto child : g[front]) {
             if(!vis[child]) {
+              vis[child] = 1;
               q.push(child);
               dist[child] = dist[front] + 1;
             }
